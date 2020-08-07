f7af8d0e095182690ed38a662e4a61e2b4a14017
wayne <zs577215@gmail.com>
2020-08-07 22:42:02 +0800

    DRY

---
+++ src/main/java/priciple/theory/gDRY/demo2/IpUtils.java
@@ -0,0 +1,55 @@
+package priciple.theory.gDRY.demo2;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * demo2: 功能语义重复
+ *
+ * 在同一个项目代码中有下面两个函数：isValidIp() 和 checkIfIpValid()。
+ * 尽管两个函数的命名不同，实现逻辑不同，但功能是相同的，都是用来判定 IP 地址是否合法的。
+ *
+ * demo1 中的例子是代码实现逻辑重复，但语义不重复，我们并不认为它违反了 DRY 原则。
+ * 而在这个例子中，尽管两段代码的实现逻辑不重复，但语义重复，也就是功能重复，我们认为它违反了 DRY 原则。
+ * 我们应该在项目中，统一一种实现思路，所有用到判断 IP 地址是否合法的地方，都统一调用同一个函数。
+ *
+ */
+public class IpUtils {
+
+    public boolean isValidIp(String ipAddress) {
+        if (StringUtils.isBlank(ipAddress)) {
+            return false;
+        }
+        String regex = "^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|[1-9])\\."
+                      + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+                      + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+                      + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)$";
+        return ipAddress.matches(regex);
+    }
+
+    public boolean checkIfIpValid(String ipAddress) {
+        if (StringUtils.isBlank(ipAddress)) {
+            return false;
+        }
+        String[] ipUnits = StringUtils.split(ipAddress, '.');
+        if (ipUnits.length != 4) {
+            return false;
+        }
+        for (int i = 0; i < 4; i++) {
+            int ipUnitIntValue;
+            try {
+                ipUnitIntValue = Integer.parseInt(ipUnits[i]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            if (ipUnitIntValue < 0 || ipUnitIntValue > 255) {
+                return false;
+            }
+            if (i == 0 && ipUnitIntValue == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ src/main/java/priciple/theory/gDRY/demo3/EmailValidation.java
@@ -0,0 +1,7 @@
+package priciple.theory.gDRY.demo3;
+
+public class EmailValidation {
+    public static boolean validate(String email) {
+        return false;
+    }
+}
